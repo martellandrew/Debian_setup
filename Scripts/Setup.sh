7701c81b18ce0d9a3db226e228a104e203229463
martellandrew <93014693+martellandrew@users.noreply.github.com>
2024-07-01 12:11:14 -0700

    Update Setup.sh
    Removed neofetch 
    Added Belina Etcher
    Added GDeb GUI

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-sudo apt install neofetch -y
+#####Belina Etcher
+wget https://github.com/balena-io/etcher/releases/download/v1.19.21/balena-etcher_1.19.21_amd64.deb
+sudo apt install ./balena-etcher_1.19.21_amd64.deb
+
+
+
+
+#####GDebi GUI
+sudo apt install gdebi -y
+
+wget http://ftp.it.debian.org/debian/pool/main/g/gconf/gconf2_3.2.6-8_amd64.deb
+wget http://ftp.it.debian.org/debian/pool/main/g/gconf/libgconf-2-4_3.2.6-8_amd64.deb
+wget http://ftp.it.debian.org/debian/pool/main/g/gconf/gconf2-common_3.2.6-8_all.deb
+wget http://ftp.it.debian.org/debian/pool/main/g/gconf/gconf-service_3.2.6-8_amd64.deb
+wget http://ftp.it.debian.org/debian/pool/main/o/openldap/libldap-2.5-0_2.5.13%2bdfsg-5_amd64.deb
+
+sudo dpkg -i gconf2-common_3.2.6-8_all.deb 
+sudo dpkg -i libgconf-2-4_3.2.6-8_amd64.deb 
+sudo dpkg -i libldap-2.5-0_2.5.13+dfsg-5_amd64.deb 
+sudo dpkg -i gconf-service_3.2.6-8_amd64.deb 
+sudo dpkg -i gconf2_3.2.6-8_amd64.deb 
